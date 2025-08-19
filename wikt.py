193bfc0aa78d0e3d83feccd376b6edadf7d2db2e
Neal <149696795+Neal-Bhattacharya@users.noreply.github.com>
2025-08-19 10:44:55 -0400

    Update wikt.py

--- wikt.py
+++ wikt.py
@@ -8,12 +8,26 @@ import sys
 # Set this to True to only list langs in `user_langs`
 limit_langs = False
 
+# Dangerous to set True!
+# see https://urllib3.readthedocs.io/en/latest/advanced-usage.html#tls-warnings
+insecure = False
+
 # These must be entered exactly as they appear in Wiktionary headings
 user_langs = ['English', 'French', 'Spanish', 'Ancient Greek', 'Latin', 'German']
 sep = "----------------------------------------"
-GREEN = "\033[32m"
-YELLOW = "\033[94m"
-ESCAPE = "\033[0m"
+
+def color(s, color):
+    GREEN = "\033[32m"
+    YELLOW = "\033[94m"
+    BLUE = "\033[94m"
+    ESCAPE = "\033[0m"
+    match color.lower():
+        case "green":
+            return GREEN + s + ESCAPE
+        case "blue":
+            return BLUE + s + ESCAPE
+        case "yellow":
+            return YELLOW + s + ESCAPE
 
 def goodbye():
     print("\nGoodbye")
@@ -23,14 +37,14 @@ def getUserWord():
     word = ""
     while word == "":
         try:
-            word = input("Enter word" + GREEN +  " (q to quit)" + ESCAPE + "\n>").strip()
+            word = input("Enter word" + color(" (q to quit)", "green")+ "\n>").strip()
         except KeyboardInterrupt:
             goodbye()
     return word
 
 def getWordJson(word):
     url = "https://en.wiktionary.org/api/rest_v1/page/definition/" + word
-    res = requests.get(url)
+    res = requests.get(url, verify=not insecure)
 
     raw_str = re.sub('<[^<]+?>', '', res.text)
     if "404" in raw_str:
@@ -71,7 +85,7 @@ def parseJson(dict_obj):
 def printOut(langs):
     print(sep)
     for k in langs.keys():
-        print("\033[94m" + k + "\033[0m")
+        print(color(k, "blue"))
         for p in langs[k].keys():
             print("  " + str(p))
             y = 1
@@ -79,6 +93,10 @@ def printOut(langs):
                 print("       " + str(y) + ". " + str(d.strip()))
                 y += 1
 def main():
+    if (insecure):
+        import urllib3
+        urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
+
     while True:
         word = getUserWord()
         if word == "q":
